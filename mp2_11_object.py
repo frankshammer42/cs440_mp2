5c41e070b54ac897f67885807c132c0b729d24f9
tt45 <thompsonteng@wirelessprv-10-194-146-31.near.illinois.edu>
2017-10-22 17:46:22 -0500

    delete the size

--- mp2_11_object.py
+++ mp2_11_object.py
@@ -1,7 +1,13 @@
 class puzzle_obj(object):
-    def __init__(size, puzzle, color_dic, blank_pos):
-        self.size = size
+    def __init__(self, puzzle, color_dic, blank_pos):
         self.puzzle = puzzle
         self.color_dic = color_dic
         self.blank_pos = blank_pos
         self.curr_pos = color_dic[list(color_dic.keys())[0]][0]
+
+    def fill_color(self, pos, color):
+        self.blank_pos.remove(pos)
+        self.puzzle[pos[0]][pos[1]] = color
+        self.curr_pos = pos
+        if pos in self.color_dic[color]:
+            self.color_dict.remove(color)
