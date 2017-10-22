288411165c0606fc8ee5cd2a2ad758eafde2ae5a
Frankshammer42 <frankshammer42@wirelessprv-10-193-197-94.near.illinois.edu>
2017-10-22 18:07:23 -0500

    merge

--- utility.py
+++ utility.py
@@ -81,38 +81,9 @@ def satisfy_constrain(color_to_fill, position_to_check, configuration):
             return True
 
 
->>>>>>> d862571dcdc14c9b73a8bf895bee7c163966f1aa
-# configure ,_ ,_ = read_puzzle('./puzzle1.txt')
-# print_configuration(configure)
-# print in_board((4, -1), configure)
-
-<<<<<<< HEAD
-=======
-def backtracking_dumb(puzzle):    #gameObject puzzle
-
-    puzzle_arr = []
-
-    puzzle_arr.append(puzzle)
-
-    while puzzle_arr.length():
-        cur_puzzle = puzzle_arr.pop()
-        if (isGoal(cur_puzzle)):
-            return cur_puzzle
-        else:
-            #check source
-
-            #check constrain
-
-                #satisfied -> fill -> append
-
-
-    return null
-
-#check if the puzzle satisfies all constrains and return a solution
 def isGoal(puzzle):
     for i in range(len(puzzle)):
         for j in range(len(puzzle[0])):
             if puzzle[i][j] == '0':
                 return False
     return True
->>>>>>> d862571dcdc14c9b73a8bf895bee7c163966f1aa
