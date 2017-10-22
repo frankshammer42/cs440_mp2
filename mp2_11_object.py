202922f0c630c17f261f3e3318e9196df998d0a4
tt45 <thompsonteng@wirelessprv-10-194-146-31.near.illinois.edu>
2017-10-22 18:06:25 -0500

    is_source added

--- mp2_11_object.py
+++ mp2_11_object.py
@@ -1,13 +1,18 @@
-class puzzle_obj(object):
-    def __init__(self, puzzle, color_dic, blank_pos):
-        self.puzzle = puzzle
+class puzzle(object):
+    def __init__(self, configuration, color_dic, blank_pos):
+        self.configuration = configuration
         self.color_dic = color_dic
         self.blank_pos = blank_pos
         self.curr_pos = color_dic[list(color_dic.keys())[0]][0]
 
     def fill_color(self, pos, color):
         self.blank_pos.remove(pos)
-        self.puzzle[pos[0]][pos[1]] = color
+        self.configuration[pos[0]][pos[1]] = color
         self.curr_pos = pos
         if pos in self.color_dic[color]:
             self.color_dict.remove(color)
+
+    def is_source(self, pos, color):
+        if pos in self.color_dict[color]:
+            return True
+        return False
